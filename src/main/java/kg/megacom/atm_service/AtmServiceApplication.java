733292758fbf263097f7bb10540d111119fb6b03
daskarbekov <dolon91aa@gmail.com>
2022-09-02 09:44:05 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/atm_service/AtmServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.atm_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/megacom/atm_service/AtmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.atm_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
